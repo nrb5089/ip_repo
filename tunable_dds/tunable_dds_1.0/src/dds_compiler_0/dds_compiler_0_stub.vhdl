44050fc0f475aeb6be5fb342536f76ecbbf65ee6
Nathan Blinn <nrb5089@gmail.com>
2025-02-24 12:24:10 -0800

    added tunabledds

---
+++ tunable_dds/tunable_dds_1.0/src/dds_compiler_0/dds_compiler_0_stub.vhdl
@@ -0,0 +1,38 @@
+-- Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2021.2 (win64) Build 3367213 Tue Oct 19 02:48:09 MDT 2021
+-- Date        : Thu Sep 19 15:55:48 2024
+-- Host        : NateLaptop running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               d:/Projects/ip_repo/tunable_dds/tunable_dds_1.0/src/dds_compiler_0/dds_compiler_0_stub.vhdl
+-- Design      : dds_compiler_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z010clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity dds_compiler_0 is
+  Port ( 
+    aclk : in STD_LOGIC;
+    aclken : in STD_LOGIC;
+    aresetn : in STD_LOGIC;
+    s_axis_phase_tvalid : in STD_LOGIC;
+    s_axis_phase_tready : out STD_LOGIC;
+    s_axis_phase_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    m_axis_data_tvalid : out STD_LOGIC;
+    m_axis_data_tready : in STD_LOGIC;
+    m_axis_data_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end dds_compiler_0;
+
+architecture stub of dds_compiler_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "aclk,aclken,aresetn,s_axis_phase_tvalid,s_axis_phase_tready,s_axis_phase_tdata[31:0],m_axis_data_tvalid,m_axis_data_tready,m_axis_data_tdata[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "dds_compiler_v6_0_21,Vivado 2021.2";
+begin
+end;
